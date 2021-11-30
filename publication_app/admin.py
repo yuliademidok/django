8bfa1c25b0b3b46298509bdd4801a810e15d6d59
yulia <nappestok@gmail.com>
2021-11-30 09:16:22 +0300

    set up

---
+++ publication_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Post
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "create_date", "title", )
+    ordering = ("-create_date", "-id", )
+    readonly_fields = ("create_date", )
---
+++ publication_app/views.py
@@ -0,0 +1,16 @@
+import random
+
+from django.shortcuts import render
+
+from .models import Post
+
+
+def main_page(request):
+    # mock_posts = [{"title": random.randint(1, 10000), "text": "some text some text some text "} for _ in range(100)]
+    posts = Post.objects.filter(is_public=True, ).order_by("-create_date", "-id").all()
+    context = {
+        "title": "Hi world",
+        "body": "Somebody",
+        "posts": posts
+    }
+    return render(request, "mainpage.html", context)
