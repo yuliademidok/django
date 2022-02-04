2a9610367637821a54a9cd39581a09ff541641f3
yulia <nappestok@gmail.com>
2022-02-04 10:16:24 +0300

    update settings

--- teach_me/settings.py
+++ teach_me/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#d5zlmu54nys+-e9b=r6d5wh!2pylwtfbg5-pop5p)06yrw_v='
+# SECRET_KEY = 'django-insecure-#d5zlmu54nys+-e9b=r6d5wh!2pylwtfbg5-pop5p)06yrw_v='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_VALUE")
 
 ALLOWED_HOSTS = ["*", "127.0.0.1"]
 
